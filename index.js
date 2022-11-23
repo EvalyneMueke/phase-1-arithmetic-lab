8842c406316be09862871615429bf3767ba698f5
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-11-23 09:22:59 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+//the answer must be 64 so we have use constant numbers that willgive us 64 
+//that is 31 and 2
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+
+//The number we are generating is random so math.ceil a random one
+let random = Math.ceil(Math.random());
+
+//the remainder should be 4
+
+const num3 = 65;
+const num4 = 14;
+let mod = Math.floor(num3/num4);
+
+//65/14 the answer 4.6 so to get the exact answer being 4 we can Math.trunc(mod);
+
+let max = Math.max(5,9,13,20);
+
+
+
